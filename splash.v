ab0ca9ae0f6481ede32bc6f0c33b888aad4d0f5f
Darwin Truong <darwin.truong@outlook.com>
2016-11-25 00:15:04 -0500

    fix splash xd

--- splash.v
+++ splash.v
@@ -7,15 +7,20 @@ module splash(
 		output reg showGameOver
 	);
 	
-	localparam TITLE = 2'b00, WAIT = 2'b01, GAMEOVER = 2'b10;
-	wire curr_state, next_state
+	localparam
+		TITLE = 2'b00, 
+		WAIT = 2'b01, 
+		GAMEOVER = 2'b10,
+		GAMEOVERWAIT = 2'b11;
+
+	reg [1:0] curr_state, next_state;
 
 	always @(*)
 	begin
 		case (curr_state)
 			TITLE: next_state = start ? TITLE : WAIT;
 			WAIT: next_state = isDead ? GAMEOVER : WAIT;
-			GAMEOVER: next_state = rst ? TITLE : GAMEOVER;
+			GAMEOVER: next_state = start ? TITLE : GAMEOVER;
 		endcase
 	end
 
@@ -34,8 +39,6 @@ module splash(
 		if (!rst)
 		begin
 			curr_state <= TITLE;
-			showTitle <= 0;
-			showGameOver <= 0;
 		end
 		else
 		begin
