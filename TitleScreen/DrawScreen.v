1487fcff4a79c8cfd993cd03bee32b1305a7d064
Darwin Truong <darwin.truong@outlook.com>
2016-11-28 02:21:05 -0500

    endscreen animation?

--- TitleScreen/DrawScreen.v
+++ TitleScreen/DrawScreen.v
@@ -1,16 +1,18 @@
 `include "ram_title.v"
+`include "ram_gameover.v"
 module DrawBlack (
 		input clk,
 		input rst,
 		input showTitle,
 		input showBlack,
 		input showGameOver,
+		input flash,
 		output reg [7:0] x,
 		output reg [6:0] y,
 		output reg [2:0] colourOut
 	);
 	
-	wire [2:0] red, black, title_ram_out;
+	wire [2:0] red, black, title_ram_out, gameover_ram_out;
 	reg [14:0] address;
 
 	assign red = 3'b100;
@@ -24,6 +26,14 @@ module DrawBlack (
 		.q(title_ram_out)
 		);
 
+	ram_gameover gameover (
+		.address(address),
+		.clock(clk),
+		.data(3'b0),
+		.wren(1'b0),
+		.q(gameover_ram_out)
+		);
+
 	always @(posedge clk, negedge rst)
 	begin
 		if (!rst)
@@ -31,6 +41,7 @@ module DrawBlack (
 			address <= 0;
 		end
 		else begin
+			if (showTitle || showGameOver || flash)
 			address <= address + 1;
 		end
 	end
@@ -43,6 +54,8 @@ module DrawBlack (
 			colourOut = black;
 		else if (showTitle)
 			colourOut = title_ram_out;
+		else if (flash)
+			colourOut = title_ram_out == 1'b100 ? black : title_ram_out;
 		x = address % 8'd160;
 		y = address / 8'd160;
 	end
--- TitleScreen/testsplash.do
+++ TitleScreen/testsplash.do
@@ -1,13 +1,16 @@
 vlib work
-vlog -timescale 1ns/1ns DrawScreen.v
-vsim -L altera_mf_ver DrawBlack
+vlog -timescale 1ns/1ns splash.v
+vsim splash
 
 log {/*}
 add wave {/*}
 
 force {clk} 0 0 ns, 1 1 ns -r 2 ns
 force {rst} 1 0 ns, 0 1 ns, 1 2 ns
-force {showTitle} 1 0ns
+force {start} 1 0 ns, 0 210 ns, 1 212 ns, 0 1000 ns, 1 1002 ns
+force {isDead} 0 0 ns, 1 500 ns
+force {tick} 0 0 ns, 1 750 ns, 0 752 ns
 
-run 40000 ns
+
+run 2000 ns
 
